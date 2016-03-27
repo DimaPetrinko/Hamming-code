8c5c6cb791ec821d518145ea402d3bbd7519e98a
Blind56 <petrinko6@gmail.com>
2016-03-27 21:50:35 +0300

    recovery

--- Hamming code/Hamming code/Form1.Designer.cs
+++ Hamming code/Hamming code/Form1.Designer.cs
@@ -28,12 +28,339 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Panel bitPanel;
+            this.bit0 = new System.Windows.Forms.NumericUpDown();
+            this.bit1 = new System.Windows.Forms.NumericUpDown();
+            this.bit2 = new System.Windows.Forms.NumericUpDown();
+            this.bit3 = new System.Windows.Forms.NumericUpDown();
+            this.bit4 = new System.Windows.Forms.NumericUpDown();
+            this.bit5 = new System.Windows.Forms.NumericUpDown();
+            this.bit6 = new System.Windows.Forms.NumericUpDown();
+            this.bit7 = new System.Windows.Forms.NumericUpDown();
+            this.bit8 = new System.Windows.Forms.NumericUpDown();
+            this.bit9 = new System.Windows.Forms.NumericUpDown();
+            this.bit10 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown11 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown12 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown13 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown14 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown15 = new System.Windows.Forms.NumericUpDown();
+            bitPanel = new System.Windows.Forms.Panel();
+            bitPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bit0)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bit1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bit2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bit3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bit4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bit5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bit6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bit7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bit8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bit9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bit10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown12)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown13)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown14)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown15)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bitPanel
+            // 
+            bitPanel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            bitPanel.Controls.Add(this.numericUpDown15);
+            bitPanel.Controls.Add(this.numericUpDown14);
+            bitPanel.Controls.Add(this.numericUpDown13);
+            bitPanel.Controls.Add(this.numericUpDown12);
+            bitPanel.Controls.Add(this.numericUpDown11);
+            bitPanel.Controls.Add(this.bit10);
+            bitPanel.Controls.Add(this.bit9);
+            bitPanel.Controls.Add(this.bit8);
+            bitPanel.Controls.Add(this.bit7);
+            bitPanel.Controls.Add(this.bit6);
+            bitPanel.Controls.Add(this.bit5);
+            bitPanel.Controls.Add(this.bit4);
+            bitPanel.Controls.Add(this.bit3);
+            bitPanel.Controls.Add(this.bit2);
+            bitPanel.Controls.Add(this.bit1);
+            bitPanel.Controls.Add(this.bit0);
+            bitPanel.Location = new System.Drawing.Point(12, 12);
+            bitPanel.Name = "bitPanel";
+            bitPanel.Size = new System.Drawing.Size(907, 61);
+            bitPanel.TabIndex = 1;
+            // 
+            // bit0
+            // 
+            this.bit0.Dock = System.Windows.Forms.DockStyle.Left;
+            this.bit0.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bit0.Location = new System.Drawing.Point(0, 0);
+            this.bit0.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.bit0.Name = "bit0";
+            this.bit0.Size = new System.Drawing.Size(30, 29);
+            this.bit0.TabIndex = 0;
+            // 
+            // bit1
+            // 
+            this.bit1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.bit1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bit1.Location = new System.Drawing.Point(30, 0);
+            this.bit1.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.bit1.Name = "bit1";
+            this.bit1.Size = new System.Drawing.Size(30, 29);
+            this.bit1.TabIndex = 1;
+            // 
+            // bit2
+            // 
+            this.bit2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.bit2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bit2.Location = new System.Drawing.Point(60, 0);
+            this.bit2.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.bit2.Name = "bit2";
+            this.bit2.Size = new System.Drawing.Size(30, 29);
+            this.bit2.TabIndex = 2;
+            // 
+            // bit3
+            // 
+            this.bit3.Dock = System.Windows.Forms.DockStyle.Left;
+            this.bit3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bit3.Location = new System.Drawing.Point(90, 0);
+            this.bit3.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.bit3.Name = "bit3";
+            this.bit3.Size = new System.Drawing.Size(30, 29);
+            this.bit3.TabIndex = 3;
+            // 
+            // bit4
+            // 
+            this.bit4.Dock = System.Windows.Forms.DockStyle.Left;
+            this.bit4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bit4.Location = new System.Drawing.Point(120, 0);
+            this.bit4.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.bit4.Name = "bit4";
+            this.bit4.Size = new System.Drawing.Size(30, 29);
+            this.bit4.TabIndex = 4;
+            // 
+            // bit5
+            // 
+            this.bit5.Dock = System.Windows.Forms.DockStyle.Left;
+            this.bit5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bit5.Location = new System.Drawing.Point(150, 0);
+            this.bit5.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.bit5.Name = "bit5";
+            this.bit5.Size = new System.Drawing.Size(30, 29);
+            this.bit5.TabIndex = 5;
+            // 
+            // bit6
+            // 
+            this.bit6.Dock = System.Windows.Forms.DockStyle.Left;
+            this.bit6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bit6.Location = new System.Drawing.Point(180, 0);
+            this.bit6.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.bit6.Name = "bit6";
+            this.bit6.Size = new System.Drawing.Size(30, 29);
+            this.bit6.TabIndex = 6;
+            // 
+            // bit7
+            // 
+            this.bit7.Dock = System.Windows.Forms.DockStyle.Left;
+            this.bit7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bit7.Location = new System.Drawing.Point(210, 0);
+            this.bit7.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.bit7.Name = "bit7";
+            this.bit7.Size = new System.Drawing.Size(30, 29);
+            this.bit7.TabIndex = 7;
+            // 
+            // bit8
+            // 
+            this.bit8.Dock = System.Windows.Forms.DockStyle.Left;
+            this.bit8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bit8.Location = new System.Drawing.Point(240, 0);
+            this.bit8.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.bit8.Name = "bit8";
+            this.bit8.Size = new System.Drawing.Size(30, 29);
+            this.bit8.TabIndex = 8;
+            // 
+            // bit9
+            // 
+            this.bit9.Dock = System.Windows.Forms.DockStyle.Left;
+            this.bit9.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bit9.Location = new System.Drawing.Point(270, 0);
+            this.bit9.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.bit9.Name = "bit9";
+            this.bit9.Size = new System.Drawing.Size(30, 29);
+            this.bit9.TabIndex = 9;
+            // 
+            // bit10
+            // 
+            this.bit10.Dock = System.Windows.Forms.DockStyle.Left;
+            this.bit10.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bit10.Location = new System.Drawing.Point(300, 0);
+            this.bit10.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.bit10.Name = "bit10";
+            this.bit10.Size = new System.Drawing.Size(30, 29);
+            this.bit10.TabIndex = 10;
+            // 
+            // numericUpDown11
+            // 
+            this.numericUpDown11.Dock = System.Windows.Forms.DockStyle.Left;
+            this.numericUpDown11.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown11.Location = new System.Drawing.Point(330, 0);
+            this.numericUpDown11.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown11.Name = "numericUpDown11";
+            this.numericUpDown11.Size = new System.Drawing.Size(30, 29);
+            this.numericUpDown11.TabIndex = 11;
+            // 
+            // numericUpDown12
+            // 
+            this.numericUpDown12.Dock = System.Windows.Forms.DockStyle.Left;
+            this.numericUpDown12.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown12.Location = new System.Drawing.Point(360, 0);
+            this.numericUpDown12.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown12.Name = "numericUpDown12";
+            this.numericUpDown12.Size = new System.Drawing.Size(30, 29);
+            this.numericUpDown12.TabIndex = 12;
+            // 
+            // numericUpDown13
+            // 
+            this.numericUpDown13.Dock = System.Windows.Forms.DockStyle.Left;
+            this.numericUpDown13.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown13.Location = new System.Drawing.Point(390, 0);
+            this.numericUpDown13.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown13.Name = "numericUpDown13";
+            this.numericUpDown13.Size = new System.Drawing.Size(30, 29);
+            this.numericUpDown13.TabIndex = 13;
+            // 
+            // numericUpDown14
+            // 
+            this.numericUpDown14.Dock = System.Windows.Forms.DockStyle.Left;
+            this.numericUpDown14.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown14.Location = new System.Drawing.Point(420, 0);
+            this.numericUpDown14.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown14.Name = "numericUpDown14";
+            this.numericUpDown14.Size = new System.Drawing.Size(30, 29);
+            this.numericUpDown14.TabIndex = 14;
+            // 
+            // numericUpDown15
+            // 
+            this.numericUpDown15.Dock = System.Windows.Forms.DockStyle.Left;
+            this.numericUpDown15.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown15.Location = new System.Drawing.Point(450, 0);
+            this.numericUpDown15.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown15.Name = "numericUpDown15";
+            this.numericUpDown15.Size = new System.Drawing.Size(30, 29);
+            this.numericUpDown15.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(931, 261);
+            this.Controls.Add(bitPanel);
+            this.Name = "Form1";
             this.Text = "Form1";
+            bitPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.bit0)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bit1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bit2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bit3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bit4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bit5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bit6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bit7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bit8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bit9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bit10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown12)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown13)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown14)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown15)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.NumericUpDown bit0;
+        private System.Windows.Forms.NumericUpDown numericUpDown15;
+        private System.Windows.Forms.NumericUpDown numericUpDown14;
+        private System.Windows.Forms.NumericUpDown numericUpDown13;
+        private System.Windows.Forms.NumericUpDown numericUpDown12;
+        private System.Windows.Forms.NumericUpDown numericUpDown11;
+        private System.Windows.Forms.NumericUpDown bit10;
+        private System.Windows.Forms.NumericUpDown bit9;
+        private System.Windows.Forms.NumericUpDown bit8;
+        private System.Windows.Forms.NumericUpDown bit7;
+        private System.Windows.Forms.NumericUpDown bit6;
+        private System.Windows.Forms.NumericUpDown bit5;
+        private System.Windows.Forms.NumericUpDown bit4;
+        private System.Windows.Forms.NumericUpDown bit3;
+        private System.Windows.Forms.NumericUpDown bit2;
+        private System.Windows.Forms.NumericUpDown bit1;
     }
 }
 
